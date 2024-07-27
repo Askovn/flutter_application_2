7095932913acca37627eb74496c9d16b683f1403
Askovn <andrey.kovnarev@gmail.com>
2024-07-27 18:07:16 +0300

    Login page

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,123 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_2/theme/theme.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool isLogin = true; // Флаг для определения режима (вход/регистрация)
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomCenter,
+            colors: isLogin
+                ? [
+                    DoDidDoneTheme.lightTheme.colorScheme.secondary,
+                    DoDidDoneTheme.lightTheme.colorScheme.primary
+                  ]
+                : [
+                    DoDidDoneTheme.lightTheme.colorScheme.primary,
+                    DoDidDoneTheme.lightTheme.colorScheme.secondary
+                  ],
+            stops: const [0.2, 0.8], // Основной цвет занимает 80%
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                isLogin ? 'Вход' : 'Регистрация',
+                style: const TextStyle(
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+              ),
+              const SizedBox(height: 30),
+              const TextField(
+                decoration: InputDecoration(
+                  hintText: 'Логин (почта)',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 15),
+              const TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'Пароль',
+                  filled: true,
+                  fillColor: Colors.white,
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+              ),
+              // Условие для отображения поля "Повторить пароль"
+              if (!isLogin)
+                const SizedBox(height: 15),
+              if (!isLogin)
+                const TextField(
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    hintText: 'Повторить пароль',
+                    filled: true,
+                    fillColor: Colors.white,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.all(Radius.circular(20)),
+                      borderSide: BorderSide.none,
+                    ),
+                  ),
+                ),
+              const SizedBox(height: 30),
+              ElevatedButton(
+                onPressed: () {
+                  // Обработка входа/регистрации
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: !isLogin
+                      ? DoDidDoneTheme.lightTheme.colorScheme.primary
+                      : DoDidDoneTheme.lightTheme.colorScheme.secondary,
+                  padding: const EdgeInsets.symmetric(horizontal: 50, vertical: 15),
+                  textStyle: const TextStyle(fontSize: 18),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                child: Text(isLogin ? 'Войти' : 'Зарегистрироваться'),
+              ),
+              const SizedBox(height: 20),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    isLogin = !isLogin;
+                  });
+                },
+                child: Text(
+                  isLogin ? 'У меня еще нет аккаунта' : 'Вернуться на вход',
+                  style: const TextStyle(color: Colors.white),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
